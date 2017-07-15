17ef2f8a7f8312bc353cb0ee5a1564cef98820db
zhoulibo <zhoulibo_vip@163.com>
2017-07-15 17:05:46 +0800

    支付宝支付

--- ivan-parent/ivan-api/src/main/java/org/ivan/api/AliPayService.java
+++ ivan-parent/ivan-api/src/main/java/org/ivan/api/AliPayService.java
@@ -1,13 +1,27 @@
 package org.ivan.api;
 
 import java.util.Map;
-
+/**
+ * 支付宝扫码支付
+ * @author 周立波
+ *
+ */
 public interface AliPayService {
 	/**
 	 * 支付宝扫码支付
+	 * @param orderNum 商户订单号
+	 * @param goodsName商品名字
+	 * @param amount 金额
+	 * @param timeOut 订单失效时间
 	 * @return
 	 * @throws Exception
 	 */
-	public Map<String,Object> alipay(Map<String,Object> map)throws Exception;
+	public Map<String,Object> alipay(String orderNum, String goodsName,double amount, int timeOut)throws Exception;
+	/**
+	 * 支付宝回调,验签
+	 * @param map
+	 * @return
+	 */
+	public  boolean verify(Map<String, String> map);
 	
 }
