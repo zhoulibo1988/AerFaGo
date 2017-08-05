53f0728157de2f8a155917774a6eaa2f14529b4a
zhoulibo <zhoulibo_vip@163.com>
2017-08-05 16:31:51 +0800

    修改类名字

---
+++ ivan-parent/ivan-dubbo-web/src/main/java/com/ivan/web/controller/admin/SysAdminController.java
@@ -0,0 +1,16 @@
+package com.ivan.web.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/admin/*")
+public class SysAdminController {
+	@RequestMapping("login")
+	public ModelAndView goToLogin(){
+		ModelAndView av=new ModelAndView("admin/login");
+		return av;
+		
+	}
+}
