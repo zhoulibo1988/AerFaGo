76a40757f8ace2266a11e793f8b58a7ff6a52eb6
zhoulibo <zhoulibo_vip@163.com>
2017-08-15 16:15:40 +0800

    admin

--- ivan-parent/ivan-entity/src/main/java/org/ivan/entity/utils/PageObject.java
+++ ivan-parent/ivan-entity/src/main/java/org/ivan/entity/utils/PageObject.java
@@ -1,103 +1,102 @@
 package org.ivan.entity.utils;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 /**
  * 该类为公共分页类,用于查询数据时用
- * @author 
- * @version 
+ * 
+ * @author
+ * @version
  */
-public class PageObject<T>
-{
+public class PageObject<T> implements Serializable{
 	/**
 	 * 首页
 	 */
 	private final int firstPage = Common.FIRST_PAGE;
-	
+
 	/**
 	 * 每页的数据量
 	 */
 	private int pageData = Common.PAGE_DATA;
-	
+
 	/**
 	 * 当前页
 	 */
 	private int curPage = Common.FIRST_PAGE;
-	
+
 	/**
 	 * 下一页
 	 */
 	private int nextPage;
-	
+
 	/**
 	 * 上一页
 	 */
 	private int prePage;
-	
+
 	/**
 	 * 最后一页
 	 */
 	private int endPage;
-	
+
 	/**
 	 * 总页数
 	 */
 	private int totalPage;
-	
+
 	/**
 	 * 总数据量
 	 */
 	private int totalData;
-	
+
 	/**
 	 * 保存的数据
 	 */
 	@SuppressWarnings("unchecked")
-    private List<T> dataList = Collections.EMPTY_LIST;
-	
-	/** 
+	private List<T> dataList = Collections.EMPTY_LIST;
+
+	/**
 	 * 页码
 	 */
 	private List<Integer> pageList = new ArrayList<Integer>();
-	
+
 	public PageObject() {
 	}
-	
-	public PageObject(int totalData,int curPage){
+
+	public PageObject(int totalData, int curPage) {
 		this.totalData = totalData;
 		this.curPage = curPage;
 		pageValue();
 	}
-	
-	public PageObject(int totalData,int curPage,int pageData){
+
+	public PageObject(int totalData, int curPage, int pageData) {
 		this.totalData = totalData;
 		this.curPage = curPage;
 		this.pageData = pageData;
 		pageValue();
 	}
-	
-	private void pageValue(){
+
+	private void pageValue() {
 		pageList.add(Common.FIRST_PAGE);
-		if (0 != totalData)
-		{
-			//得到总页数以及最后一页
+		if (0 != totalData) {
+			// 得到总页数以及最后一页
 			this.endPage = this.totalPage = totalData % pageData == 0 ? totalData / pageData : totalData / pageData + 1;
-			for (int i = 2;i <= endPage;i ++)
-			{
+			for (int i = 2; i <= endPage; i++) {
 				pageList.add(i);
 			}
-			
-			//判断当前页的页码是否合法
+
+			// 判断当前页的页码是否合法
 			this.curPage = curPage > totalPage ? totalPage : curPage;
-			//得到前一页的页码
+			// 得到前一页的页码
 			this.prePage = curPage < 2 ? firstPage : curPage - 1;
-			//得到后一页的页码
+			// 得到后一页的页码
 			this.nextPage = curPage == totalPage ? totalPage : curPage + 1;
 		}
 	}
-	
+
 	public int getFirstPage() {
 		return firstPage;
 	}
@@ -133,11 +132,9 @@ public class PageObject<T>
 	public List<T> getDataList() {
 		return dataList;
 	}
-	
-	public void setDataList(List<T> dataList)
-	{
-		if (null != dataList && !dataList.isEmpty())
-		{
+
+	public void setDataList(List<T> dataList) {
+		if (null != dataList && !dataList.isEmpty()) {
 			this.dataList = dataList;
 		}
 	}
