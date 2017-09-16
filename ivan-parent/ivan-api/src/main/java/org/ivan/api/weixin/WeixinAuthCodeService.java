8b5c8414a4aad2fff8d89f2ed1d7e7e845a4ef7a
zhoulibo <zhoulibo_vip@163.com>
2017-09-16 15:37:36 +0800

    修改

--- ivan-parent/ivan-api/src/main/java/org/ivan/api/weixin/WeixinAuthCodeService.java
+++ ivan-parent/ivan-api/src/main/java/org/ivan/api/weixin/WeixinAuthCodeService.java
@@ -1,28 +1,12 @@
 package org.ivan.api.weixin;
 
-import java.util.Map;
-
 import org.ivan.entity.WeixinAuthCode;
-import org.ivan.entity.admin.SysUcenterApps;
-import org.ivan.entity.utils.PageObject;
 
 
 /**
  * @author cyl
  * @version 
  */
-public interface WeixinAuthCodeService{
-	public void insert(WeixinAuthCode weixinAuthCode);
+public interface WeixinAuthCodeService extends BaseService<WeixinAuthCode>{
 	
-	public WeixinAuthCode selectSingle(Map<String,Object> map);
-	
-	public WeixinAuthCode selectSingle(WeixinAuthCode weixinAuthCode);
-	
-	public void updateByEntity(WeixinAuthCode weixinAuthCode);
-	 /**
-     * 分页获取
-     * @param map
-     * @return
-     */
-    PageObject<WeixinAuthCode> Pagequery(Map<String,Object> map);
 }
--- ivan-parent/ivan-api/src/main/java/org/ivan/api/weixin/WeixinBusinessInfoService.java
+++ ivan-parent/ivan-api/src/main/java/org/ivan/api/weixin/WeixinBusinessInfoService.java
@@ -9,9 +9,6 @@ import org.ivan.entity.WeixinBusinessInfo;
  * @author cyl
  * @version 
  */
-public interface WeixinBusinessInfoService  {
-	public void  insert(WeixinBusinessInfo WeixinBusinessInfo);
-	public WeixinBusinessInfo selectSingle(Map<String,Object> map);
-	public WeixinBusinessInfo selectSingle(WeixinBusinessInfo WeixinBusinessInfo);
-	public void updateByEntity(WeixinBusinessInfo WeixinBusinessInfo);
+public interface WeixinBusinessInfoService  extends BaseService<WeixinBusinessInfo>{
+	
 }
