8c25f2a2f948a68766f994f3c343621d8ade3c91
zhoulibo <zhoulibo_vip@163.com>
2017-09-26 15:15:05 +0800

    集成ES搜索

---
+++ ivan-parent/ivan-core/src/main/java/org/ivan/core/elasticsearch/EsFactory.java
@@ -0,0 +1,37 @@
+package org.ivan.core.elasticsearch;
+
+import org.apache.commons.pool2.BasePooledObjectFactory;
+import org.apache.commons.pool2.PooledObject;
+import org.apache.commons.pool2.impl.DefaultPooledObject;
+
+//链接工厂，池子的一些方法由我们实现
+public class EsFactory extends BasePooledObjectFactory<Es> {
+
+	// 创建对象
+	@Override
+	public Es create() throws Exception {
+		System.err.println("new es !!!!");
+		return new Es();
+	}
+
+	// 包装对象
+	@Override
+	public PooledObject<Es> wrap(Es arg0) {
+		return new DefaultPooledObject<Es>(arg0);
+	}
+
+	// 销毁对象关闭链接
+	@Override
+	public void destroyObject(PooledObject<Es> p) throws Exception {
+		p.getObject().close();
+		System.err.println("destory es");
+		super.destroyObject(p);
+	}
+
+	// 校验对象是否正常
+	@Override
+	public boolean validateObject(PooledObject<Es> p) {
+		System.err.println("validate es");
+		return p.getObject().validate();
+	}
+}
