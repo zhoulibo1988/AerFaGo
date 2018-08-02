a073a2cfbe1e0d05fb227ec7025f427758aa46e3
zhoulibo <zhoulibo_vip@163.com>
2018-08-02 20:07:03 +0800

    新增IT资料库

--- ivan-parent/ivan-dubbo-web/src/main/java/com/ivan/web/controller/system/SysController.java
+++ ivan-parent/ivan-dubbo-web/src/main/java/com/ivan/web/controller/system/SysController.java
@@ -1,14 +1,25 @@
 package com.ivan.web.controller.system;
 
+import io.swagger.annotations.ApiOperation;
+
+import java.util.Map;
+
+import org.ivan.api.it.ItDataService;
+import org.ivan.entity.it.ItData;
+import org.ivan.entity.utils.PageObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.alibaba.dubbo.config.annotation.Reference;
 
 @Controller
 @RequestMapping("/sys/*")
 public class SysController {
-    
+	@Reference
+	private ItDataService itDataService;
     
     @RequestMapping(value = "/login",method = { RequestMethod.GET,RequestMethod.POST })
     public ModelAndView gotoLogin() {
