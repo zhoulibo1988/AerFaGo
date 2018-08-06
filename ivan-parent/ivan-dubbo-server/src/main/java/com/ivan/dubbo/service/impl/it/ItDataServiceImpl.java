6923c66bf1491f38a827a7af4f56d7d52c6278a2
zhoulibo1988 <zhoulibo_vip@163.com>
2018-08-06 21:19:24 +0800

    xiu

--- ivan-parent/ivan-dubbo-server/src/main/java/com/ivan/dubbo/service/impl/it/ItDataServiceImpl.java
+++ ivan-parent/ivan-dubbo-server/src/main/java/com/ivan/dubbo/service/impl/it/ItDataServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.ivan.api.it.ItDataService;
-import org.ivan.entity.admin.SysUcenterAdmin;
 import org.ivan.entity.it.ItData;
 import org.ivan.entity.utils.PageHelper;
 import org.ivan.entity.utils.PageObject;
@@ -24,36 +23,30 @@ public class ItDataServiceImpl implements ItDataService {
     @Autowired
     private ItDataMapper itDataMapper;
 
-	@Override
 	public void insert(ItData t) {
 		itDataMapper.insertByEntity(t);
 	}
 
-	@Override
 	public ItData selectSingle(Map<String, Object> map) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public ItData selectSingle(ItData t) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public void updateByEntity(ItData t) {
 		itDataMapper.updateByEntity(t);
 		
 	}
 
-	@Override
 	public void updateByEntity(Map<String, Object> map) {
 		
 		
 	}
 
-	@Override
 	public PageObject<ItData> Pagequery(Map<String, Object> map) {
 		if(!map.containsKey("curPage")&&!map.containsKey("pageData")){
     		map.put("curPage", 1);
@@ -67,19 +60,16 @@ public class ItDataServiceImpl implements ItDataService {
 		return pageObject;
 	}
 
-	@Override
 	public List<ItData> getList(Map<String, Object> map) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public void del(Map<String, Object> map) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void del(ItData t) {
 		// TODO Auto-generated method stub
 		
