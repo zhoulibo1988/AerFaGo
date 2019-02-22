05027585818deca6e3c2e5ffed4f2d7dccf11455
zhoulibo <zhoulibo_vip@163.com>
2019-02-22 22:46:06 +0800

    修改

---
+++ ivan-parent/ivan-core/src/main/java/org/ivan/core/pdf/PdfUtill.java
@@ -0,0 +1,13 @@
+package org.ivan.core.pdf;
+
+/**
+ * 
+ * @ClassName: PdfUtill
+ * @Description: pdf文件在线观看
+ * @author Mr.zhou
+ * @date 2018年10月19日 下午2:43:20
+ *
+ */
+public class PdfUtill {
+	
+}
